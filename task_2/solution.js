4c0263af359d2655fffe884069a373b542fb99b6
bobby777-01 <bobby777.01@gmail.com>
2022-11-07 01:53:36 +0300

    Обратно

--- task_2/solution.js
+++ task_2/solution.js
@@ -15,8 +15,7 @@ function calcShipping(sum, min, shipping) {
 
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
-    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!то shippingSum присвоить значение shippingPrice
-    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!то shippingSum присвоить значение shippingPrice
+    
     // Конец решения задания №2.1.
 
     return shippingSum;
